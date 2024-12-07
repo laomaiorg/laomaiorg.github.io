b1deb334a53e51fc5394bab3cf64bf7e6c227fcd
laomaiorg <laomaiorg@users.noreply.github.com>
2024-12-07 17:46:59 +0000

    deploy: laomaiorg/www.iamlm.com@2aff7daa4076ef5a1ddcd2485da4d6633b56d672

---
+++ _astro/Featured.astro_astro_type_script_index_0_lang.CztZ4_gz.js
@@ -0,0 +1,1 @@
+import{u as e}from"./relativeTime.Cw4JtWda.js";document.addEventListener("astro:page-load",()=>{e()});
--- _astro/hoisted.CgBaNDGR.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.rrRgbaP2.js";import{u as e}from"./relativeTime.Cw4JtWda.js";document.addEventListener("astro:page-load",()=>{e()});
